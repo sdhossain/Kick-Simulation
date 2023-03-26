1b1ea2fbbefa0a22940f3cead34a9fa4e272763e
isamohideen <68524428+isamohideen@users.noreply.github.com>
2023-03-26 17:41:08 -0400

    Update quad/shank insertion point + thigh length

--- muscle_length.py
+++ muscle_length.py
@@ -4,9 +4,9 @@ sys.path.append('./muscle_modelling')
 
 import numpy as np
 
-QUAD_SHANK_INSERTION = (0.2, 0.03)
+QUAD_SHANK_INSERTION = (0.08, 0.03)
 KNEE_ORIGIN = (0, 0.06)
-THIGH_LENGTH = 0.5
+THIGH_LENGTH = 0.5004
 PHI = (3*np.pi)/2
 
 def quad_muscle_length(theta, thigh_offset):
